c3079c2e305fa0fbf7ac7ce2c3718b02b7211dea
cmartin <carmen.martin@iesnervion.es>
2022-10-31 10:16:05 +0100

    commit2

--- src/ejercicios/Ejercicio5.java
+++ src/ejercicios/Ejercicio5.java
@@ -1,5 +1,7 @@
 package ejercicios;
 
+import java.util.Scanner;
+
 public class Ejercicio5 {
     /*
     Realiza un programa donde el usuario "piensa" un número del 1 al 100 y el ordenador intenta adivinarlo. Es decir,
@@ -8,6 +10,32 @@ public class Ejercicio5 {
      */
 
     public static void main(String[] args) {
+        int random;
+        int randomMayor;
+        String respuesta;
+
+        Scanner sc = new Scanner(System.in);
+
+        do {
+            random = (int) (Math.random()*100);
+
+            System.out.println("Es " + random + " el numero que estas pensando?");
+            respuesta=sc.next();
+
+            if ("mayor".equals(respuesta)) {
+
+
+            } else if ("menor".equals(respuesta)) {
+                random = (int) (Math.random() * random);
+            } else{
+                System.out.println("Respuesta no valida");
+            }
+
+        }while (!respuesta.equals("igual"));
+
+        System.out.println("Es el número!!!!");
+
+
 
     }
 }
